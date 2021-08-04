c79c71dc03e1d538f01d4b1b0f5110fe57ae9fbf
Alexei <slgaming@mail.ru>
2021-08-04 21:17:40 +0300

    function test_async implemented

--- main.py
+++ main.py
@@ -5,13 +5,21 @@ import math
 async def calculate_sin(number: int, degree: int):
     print(f'Task {number} start')
     result = math.sin(degree * math.pi / 180)
-    await asyncio.sleep(0)
-    print(result)
+    if number != 5:
+        await asyncio.sleep(0)
+    print(f'---------------------\n'
+          f'Task {number} end; '
+          f'Result = {result:.5f}; '
+          f'Degree = {degree}')
 
 
 async def test_async():
-    pass
+    tasks = [
+        asyncio.create_task(calculate_sin(int(x / 30) + 1, x)) for x in range(0, 360, 30)
+    ]
+    for t in tasks:
+        await t
 
 
 if __name__ == '__main__':
-    test_async()
+    asyncio.run(test_async())
