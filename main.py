ca07f8758590d93294474e31467b57521f1c9848
Alexei <slgaming@mail.ru>
2021-08-04 21:05:16 +0300

    function calculate_sin implemented

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import asyncio
+import math
 
 
-async def calculate_sin():
-    pass
+async def calculate_sin(number: int, degree: int):
+    print(f'Task {number} start')
+    result = math.sin(degree * math.pi / 180)
+    await asyncio.sleep(0)
+    print(result)
 
 
-async def main():
+async def test_async():
     pass
 
 
 if __name__ == '__main__':
-    main()
+    test_async()
